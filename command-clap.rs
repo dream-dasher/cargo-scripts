2ffc860b623c23a3de591d900ff00218d5b58adf
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-26 19:43:22 +0100

    feat(xp): exploring shell-adj process comms

---
+++ command-clap.rs
@@ -0,0 +1,48 @@
+#!/usr/bin/env cargo +nightly -Zscript
+---
+[dependencies]
+clap = { version = "4.5", features = ["derive"] }
+---
+//! Command xp
+//! use with `BROWSER` env var
+//!
+//! ## Run:
+//!
+//! ## Convenience note:
+//! `chmod u+x opener-example.rs`
+
+use clap::Parser;
+use std::process::{Command, Stdio};
+
+/// Use with `BROWSWER` envar
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Optional string to print
+    opt_argument: Option<String>,
+    opt_runner: Option<String>,
+}
+
+const DEFAULT_ARGUMENT: &str = "Hi there, from default string.";
+const DEFAULT_RUNNER: &str = "echo";
+
+fn main() -> core::result::Result<(), Box<dyn core::error::Error>>{
+    let args = Args::parse();
+    let runner = args.opt_runner.unwrap_or(DEFAULT_RUNNER.to_string());
+    let argument = args.opt_argument.unwrap_or(DEFAULT_ARGUMENT.to_string());
+
+    println!("Argument: {}", argument);
+    println!("Runner: {}", runner);
+
+        let proc_handle = Command::new(runner)
+            .arg(argument)
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
+        let out = proc_handle.wait_with_output()?;
+        println!("Spawned process output:\n{:#?}", out);
+
+
+        Ok(())
+}
