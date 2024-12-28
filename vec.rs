93cabf6a52364f65c38c0bcb37af438d1e0a991e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-29 00:01:02 +0100

    feat: vector leak and lifetimes

--- vec.rs
+++ vec.rs
@@ -6,11 +6,50 @@
 //!
 
 fn main() {
-        println!("Hello from a rust script!");
-        let mut vv = vec!['a','b','c'];
-        println!("swap_remove(1): {}", vv.swap_remove(1));
-        println!("swap_remove(1): {}", vv.swap_remove(1));
-        // println!("swap_remove(1): {}", vv.swap_remove(1)); <-- would panic, out of bounds}
-        println!("swap_remove(0): {}", vv.swap_remove(0)); // <-- allowed despite lack of element to swap in
-        println!("swap_remove(0): {}", vv.swap_remove(0)); // <-- would panic, out of bounds}
+
+        // Swap_Remove
+        {
+                println!("Hello from a rust script!");
+                let mut vv = vec!['a','b','c'];
+                println!("swap_remove(0): {}", vv.swap_remove(0));
+                println!("last is first: {:?}", vv);
+                println!("swap_remove(1): {}", vv.swap_remove(1));
+                // println!("swap_remove(1): {}", vv.swap_remove(1)); <-- would panic, out of bounds}
+                println!("swap_remove(0): {}", vv.swap_remove(0)); // <-- allowed despite lack of element to swap in
+                // println!("swap_remove(0): {}", vv.swap_remove(0)); // <-- would panic, out of bounds}
+        }
+
+        // Leak
+        {
+                let mut ref_to_forever = {
+                        let vv = vec!['a', 'b', 'g'];
+                        vv.leak()
+                };
+                println!("leaked ref: {:?}", ref_to_forever);
+                ref_to_forever.swap(0,2);
+                println!("leaked ref: {:?}", ref_to_forever);
+                std::thread::spawn(move|| {
+                        for c in ref_to_forever {
+                                eprintln!("from thread: {}", c);
+                        }
+                });
+
+                // // Won't live long enough!
+                // let mut regular_lifetime = {
+                //         let mut vv = vec!['a', 'b', 'g'];
+                //         &mut vv
+                // };
+                let mut vv = vec!['A', 'B', 'G'];
+                let mut regular_lifetime = &mut vv;
+                println!("leaked ref: {:?}", regular_lifetime);
+                regular_lifetime.swap(0,2);
+                println!("leaked ref: {:?}", regular_lifetime);
+                // // Won't live long enough!
+                // std::thread::spawn(|| {
+                //         for c in regular_lifetime{
+                //                 println!("from thread: {}", c);
+                //         }
+                // });
+                std::thread::sleep(std::time::Duration::from_millis(10));
+        }
 }
