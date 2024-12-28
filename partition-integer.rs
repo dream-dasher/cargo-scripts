0ce21a06071c71974f9c72227b1feaea8d29e344
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-28 01:52:43 -0500

    feat(wip): integer partition; redundant; non perf

---
+++ partition-integer.rs
@@ -0,0 +1,65 @@
+#!/usr/bin/env cargo +nightly -Zscript
+---
+[dependencies]
+clap = { version = "4.5", features = ["derive"] }
+opener = "0.7.2"
+itertools = "0.13"
+---
+//! Number of ways to sum an integer in parts, ignoring order
+//! [wikipedia link: Integer Partition](https://en.wikipedia.org/wiki/Integer_partition)
+//!
+//!
+//! 5  
+//! 4 1        - 4
+//! 3 2  - 3     3 1  - 3
+//! 2 3    2 1   2 1    2 1  - 2
+//! 1 4                         1 1
+//!
+//!
+//! ## Run:
+//! `./partition-integer.rs`
+//! 
+//! ## Convenience note:
+//! `chmod u+x partition-integer.rs`
+
+use clap::Parser;
+use itertools::Itertools;
+
+/// Use `BROWSER` envvar to test `opener` crate function.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Integer to partition.
+    integer: u8,
+}
+
+fn main() {
+        let args = Args::parse();
+        let sol_vec = partition_single_int(args.integer);
+        println!("sollution with redundancies: {:?}", sol_vec);
+        todo!("Logic error: redundant solutions.  Perf error: lots of repeated work.")
+}
+
+pub fn partition_single_int(int: u8) -> Vec<Vec<u8>> {
+        if int == 0 {
+                return vec![vec![0]];
+        }
+        if int == 1 {
+                return vec![vec![1]];
+        }
+        if int == 2 {
+                return vec![vec![2], vec![1, 1]];
+        }
+
+        let mut sol = vec![vec![int]];
+        for i in int.div_ceil(2)..int {
+                let g_sol = partition_single_int(int - i);
+                let l_sol = partition_single_int(i);
+                for (mut gv, mut lv) in g_sol.into_iter().cartesian_product(l_sol.into_iter()) {
+                        gv.append(&mut lv);
+                        sol.push(gv);
+                }
+        }
+        sol
+}
+
