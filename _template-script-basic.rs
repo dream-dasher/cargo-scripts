2709e90506885b1ec658e3094deb6aa286d97e64
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-29 14:21:02 -0500

    chore(asst): templated text in templates

--- _template-script-basic.rs
+++ _template-script-basic.rs
@@ -2,9 +2,24 @@
 ---
 [dependencies]
 ---
-//! Mod Doc
+//! # Cargo-Script: {{sd_me: script_name}}
+//! 
+//! ## Convenience Section
 //!
+//! ### Shell Commands
+//! - run directly
+//!   - `chmod u+x {{sd_me: script_name}}.rs`
+//!   - `./{{sd_me: script_name}}.rs`
+//! - run via cargo
+//!   - `cargo +nightly -Zsscript {{sd_me: script_name}}.rs`
+//! - run other commands on script
+//!   - `cargo +nightly COMMAND *ARGS --manifest-path {{sd_me: script_name}}.rs -Zscript`
+//!     - e.g. `cargo +nightly update --manifest-path {{sd_me: script_name}}.rs -Zscript`
+//!
+//! ### Links
+//! - [Cargo Book: Script](https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#script)
+//! - [Github: Cargo-Script Tracking](https://github.com/rust-lang/cargo/issues/12207)
 
 fn main() {
-        println!("Hello from a rust script!");
+        println!("Hello from {{sd_me: script_name}}.rs!");
 }
--- _template-script-clap.rs
+++ _template-script-clap.rs
@@ -3,21 +3,40 @@
 [dependencies]
 clap = { version = "4.5", features = ["derive"] }
 ---
-//! Mod Doc
+//! # Cargo-Script: {{sd_me: script_name}}
+//! 
+//! ## Convenience Section
 //!
+//! ### Shell Commands
+//! - run directly
+//!   - `chmod u+x {{sd_me: script_name}}.rs`
+//!   - `./{{sd_me: script_name}}.rs`
+//! - run via cargo
+//!   - `cargo +nightly -Zsscript {{sd_me: script_name}}.rs`
+//! - run other commands on script
+//!   - `cargo +nightly COMMAND *ARGS --manifest-path {{sd_me: script_name}}.rs -Zscript`
+//!     - e.g. `cargo +nightly update --manifest-path {{sd_me: script_name}}.rs -Zscript`
+//!
+//! ### Links
+//! - [Cargo Book: Script](https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#script)
+//! - [Github: Cargo-Script Tracking](https://github.com/rust-lang/cargo/issues/12207)
 
 use clap::Parser;
 
-/// Description.
+/// {{sd_me: script_name}} Cargo-Script
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
         /// String Arg
         argument: String,
+        /// Boolean Flag
+        #[arg(short, long)]
+        wink: bool
 }
 
-fn main() -> core::result::Result<(), Box<dyn core::error::Error>>{
+fn main() {
         let args = Args::parse();
+        println!("Hi from {{sd_me: script_name}}.rs.  You said: {}", args.argument);
 
-        Ok(())
+        if args.wink { println!(";)"); }
 }
