7db461279671eee54f181d68bef9f30f20309bd7
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-01 18:49:43 -0500

    wip

--- cli-accessories.rs
+++ cli-accessories.rs
@@ -3,24 +3,34 @@
 package.edition = "2024"
 [dependencies]
 clap = { version = "4", features = ["derive", "env"] }
+config = "0.15.4"
 directories = "5.0.1"
 dotenvy = "0.15.7"
 owo-colors = "4.1.0"
 rexpect = { version = "0.6.0", features = ["which"] }
 toml = "0.8.19"
+termtree = "0.5.1"
 ---
-// config = "0.15.4"
 // human-panic = "2.0.2"
 //! # Cargo-Script: cli-accessories.rs
-//! 
-use std::{env, error::Error, result::Result};
+//!
+use std::{env, error::Error, fs::File, path::PathBuf, result::Result};
 
+use std::collections::HashMap;
+use config::Config;
 use clap::Parser;
 
 fn main() -> Result<(), Box<dyn Error>> {
         let args = Args::parse();
-        println!("Hi from cli-accessories.rs.rs.  You said: {}", args.argument);
         if args.wink { println!(";)"); }
+        let config_path = args.config_file.unwrap_or("rustfmt.toml".into());
+
+        let settings = Config::builder()
+            .add_source(config::File::with_name(config_path.to_str().expect("Path expected UTF-8")))
+            .build()
+            .unwrap();
+        println!("settings: {:#?}", settings);
+
         Ok(())
 }
 
@@ -29,7 +39,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 #[command(version, about)]
 struct Args {
         /// String Arg
-        argument: String,
+        // config_file: Vec<PathBuf>,
+        config_file: Option<PathBuf>,
         /// Boolean Flag
         #[arg(short, long)]
         wink: bool
