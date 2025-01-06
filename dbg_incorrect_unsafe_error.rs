a257cf16d876da487d4f16dc5d3a9479351b0b04
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-06 13:56:18 +0100

    docs: version ifo

--- dbg_incorrect_unsafe_error.rs
+++ dbg_incorrect_unsafe_error.rs
@@ -10,6 +10,11 @@ tracing-subscriber = "0.3.19"
 //! # Debug Example
 //!  rust-analyzer incorrect report
 //! 
+//! rust-analyzer 1.86.0-nightly (b3b368a1 2025-01-05)
+//! rustc 1.86.0-nightly (b3b368a18 2025-01-05)
+//! Zed 0.167.1 – /Applications/Zed.app
+//! helix 25.1 (dabfb6ce)
+//! 
 //! rust-analyzer incorrectly reports that there is an unsafe function call - BUT only within ide.
 //! There's no error when using `cargo check --example` or `cargo clippy --example`
 //! The error is reported for the call to create an array with a fixed value.
