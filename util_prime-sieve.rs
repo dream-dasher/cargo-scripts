5cfefaae51313dcb65b931205ce718522067df73
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-02-28 14:40:46 -0500

    feat: add option to show prime calc time
    There's enough timing fuzz and buffer from cargo
    script ui and display that the relatively simple
    elapsed time is useful to get a sense of perf.

--- util_prime-sieve.rs
+++ util_prime-sieve.rs
@@ -31,7 +31,7 @@ owo-colors = "4.1"
 //! ### Links
 //! - [Cargo Book: Script](https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#script)
 //! - [Github: Cargo-Script Tracking](https://github.com/rust-lang/cargo/issues/12207)
-use std::{error::Error, result::Result};
+use std::{error::Error, result::Result, time::Instant};
 
 use clap::Parser;
 use owo_colors::OwoColorize;
@@ -54,54 +54,61 @@ struct Args {
 
         /// Show all primes found
         #[arg(short, long)]
-        show: bool
+        show: bool,
+        /// Show timing of core prime calculation and reaping. (Ignoring UI and display times.)
+        #[arg(short, long = "time")]
+        time_calc:    bool,
 }
 fn main() -> Result<(), Box<dyn Error>> {
-        const DEFAULT_PRIMES_TILL: usize = 12_345;
-        let args = Args::parse();
-        let primes_from_or_default = args.primes_from.unwrap_or(0);
-        let primes_till_or_default = match args.primes_until {
-                None => {
-                        println!(
-                                "No `{}` input given, defaulting to : {}",
-                                "primes_until".green(),
-                                DEFAULT_PRIMES_TILL.cyan()
-                        );
-                        DEFAULT_PRIMES_TILL
-                }
-                Some(p) => {
-                        println!("You requested primes up to: {}", p.blue());
-                        p
-                }
-        };
-        println!(
-                "Calculating primes from ({}..={})...",
-                primes_from_or_default.blue(),
-                primes_till_or_default.blue()
-        );
-        if primes_from_or_default > primes_till_or_default {
-                Err("Error: your minimum is larger than your maximum.  Cancelling search.")?
-        };
-
-        let found_primes = prime_sieve(args.primes_from, primes_till_or_default);
-        println!(
-                "Number of primes found <= {}: {}",
-                primes_till_or_default.blue(),
-                found_primes.len().green().bold()
-        );
-        println!(
-                "which makes the range ({}..={}) {:.1}% prime.",
-                primes_from_or_default.blue(),
-                primes_till_or_default.blue(),
-                (100. * (found_primes.len() as f32)
-                        / ((primes_till_or_default - primes_from_or_default) as f32 + 2.))
-                        .cyan()
-                        .bold()
-        );
-        if args.show {
-                println!("{:?}", found_primes.magenta());
+const DEFAULT_PRIMES_TILL: usize = 12_345;
+let args = Args::parse();
+let primes_from_or_default = args.primes_from.unwrap_or(0);
+let primes_till_or_default = match args.primes_until {
+        None => {
+                println!(
+                        "No `{}` input given, defaulting to : {}",
+                        "primes_until".green(),
+                        DEFAULT_PRIMES_TILL.cyan()
+                );
+                DEFAULT_PRIMES_TILL
         }
-        Ok(())
+        Some(p) => {
+                println!("You requested primes up to: {}", p.blue());
+                p
+        }
+};
+println!(
+        "Calculating primes from ({}..={})...",
+        primes_from_or_default.blue(),
+        primes_till_or_default.blue()
+);
+if primes_from_or_default > primes_till_or_default {
+        Err("Error: your minimum is larger than your maximum.  Cancelling search.")?
+};
+let start_time = Instant::now();
+let found_primes = prime_sieve(args.primes_from, primes_till_or_default);
+let finish_duration = start_time.elapsed();
+println!(
+        "Number of primes found <= {}: {}",
+        primes_till_or_default.blue(),
+        found_primes.len().green().bold()
+);
+println!(
+        "which makes the range ({}..={}) {:.1}% prime.",
+        primes_from_or_default.blue(),
+        primes_till_or_default.blue(),
+        (100. * (found_primes.len() as f32)
+                / ((primes_till_or_default - primes_from_or_default) as f32 + 2.))
+                .cyan()
+                .bold()
+);
+if args.show {
+        println!("{:?}", found_primes.magenta());
+}
+if args.time_calc {
+        println!("Time taken: {:?}", finish_duration.red());
+}
+Ok(())
 }
 
 /// I'll be surprised if this works efficiently as a mechanical, literal, procedure.
